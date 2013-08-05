903618440d020cc29c252ff71e8568efee155e2b
Felix C. Stegerman <flx@obfusk.net>
2013-08-05 17:09:04 +0200

    new maven project

---
+++ src/main/java/com/github/obfusk/napp/hello/jetty/app/App.java
@@ -0,0 +1,13 @@
+package com.github.obfusk.napp.hello.jetty.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
